b2d5d38f0466b2259bd6ec793812753d94aa4103
AaronV77 <aaronvaloroso77@gmail.com>
2019-11-03 20:49:36 -0600

    Atempt #19

--- tests/basic_functionality.c
+++ tests/basic_functionality.c
@@ -8,6 +8,7 @@ START_TEST(test_queue_fill) {
     queue * q = create_queue();
     push(q, 10);
     number = pop(q);
+    printf("The number: %d\n", number);
     ck_assert_int_eq(number, 20);
     queue_cleanup(q);
 } END_TEST
